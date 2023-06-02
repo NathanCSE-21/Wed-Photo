01a59418f103cf2a2df374eabaa38a4ef0a57ea2
Nathan Nguyen <55767560+NathanCSE-21@users.noreply.github.com>
2023-06-01 22:23:10 -0400

    commit

--- script.js
+++ script.js
@@ -1,82 +1,56 @@
-const imageContainer = document.getElementById("image-container");
-const paginationContainer = document.getElementById("pagination");
+const itemsPerPage = 12; // Number of images to display per page
+const totalImages = 219; // Total number of images
+let currentPage = 1; // Current page
 
-const imagesPerPage = 12; // Number of images to display per page
-let currentPage = 1;
-let images = [];
+function fetchImages(page) {
+  const imageContainer = document.getElementById('image-container');
+  const paginationContainer = document.querySelector('.pagination');
 
-// Fetch images from the "photos" folder
-fetchImages();
+  // Store the buttons HTML in variables
+  const prevButtonHTML = '<button id="prev-btn">Back</button>';
+  const nextButtonHTML = '<button id="next-btn">Next</button>';
 
-// Function to fetch images from the "photos" folder
-function fetchImages() {
-  // Make an AJAX request or fetch API call to retrieve image URLs from the server
-  // You can use server-side technologies like Node.js, PHP, etc., to read the images from the folder and send them as a response
-  // For demonstration purposes, we'll use a static array of image URLs
+  // Clear previous content
+  imageContainer.innerHTML = '';
+  paginationContainer.innerHTML = prevButtonHTML + nextButtonHTML;
 
-  // Replace this with your server-side logic to retrieve image URLs
-  const imageUrls = [
-    "photos/image1.jpg",
-    "photos/image2.jpg",
-    "photos/image3.jpg",
-    // Add more image URLs here
-  ];
+  // Calculate start and end index of images to display on the current page
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = page * itemsPerPage;
 
-  images = imageUrls;
-  renderImages();
-}
+  // Generate image elements and append them to the image container
+  for (let i = startIndex; i < endIndex && i < totalImages; i++) {
+    const image = document.createElement('img');
+    image.classList.add('image');
+    image.src = `photos/image${i + 1}.jpg`;
+    image.alt = `Image ${i + 1}`;
+    imageContainer.appendChild(image);
+  }
 
-// Function to render the images on the page
-function renderImages() {
-  // Clear the image container
-  imageContainer.innerHTML = "";
+  // Generate Next and Back buttons
+  const prevButton = document.getElementById('prev-btn');
+  const nextButton = document.getElementById('next-btn');
 
-  // Calculate the starting and ending indexes of the current page
-  const startIndex = (currentPage - 1) * imagesPerPage;
-  const endIndex = startIndex + imagesPerPage;
+  // Enable/disable buttons based on current page
+  prevButton.disabled = page === 1;
+  nextButton.disabled = page === Math.ceil(totalImages / itemsPerPage);
 
-  // Slice the array of images based on the current page
-  const currentImages = images.slice(startIndex, endIndex);
-
-  // Loop through the current images and create HTML elements to display them
-  currentImages.forEach((imageUrl) => {
-    const imageItem = document.createElement("div");
-    imageItem.classList.add("image-item");
-
-    const image = document.createElement("img");
-    image.src = imageUrl;
-
-    imageItem.appendChild(image);
-    imageContainer.appendChild(imageItem);
+  // Event listener for previous button
+  prevButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      fetchImages(currentPage);
+    }
   });
 
-  // Render pagination
-  renderPagination();
-}
-
-// Function to render the pagination
-function renderPagination() {
-  // Clear the pagination container
-  paginationContainer.innerHTML = "";
-
-  // Calculate the total number of pages based on the number of images
-  const totalPages = Math.ceil(images.length / imagesPerPage);
-
-  // Create pagination buttons
-  for (let i = 1; i <= totalPages; i++) {
-    const pageButton = document.createElement("button");
-    pageButton.innerText = i;
-    pageButton.classList.add("page-button");
-
-    if (i === currentPage) {
-      pageButton.classList.add("active");
+  // Event listener for next button
+  nextButton.addEventListener('click', () => {
+    if (currentPage < Math.ceil(totalImages / itemsPerPage)) {
+      currentPage++;
+      fetchImages(currentPage);
     }
-
-    pageButton.addEventListener("click", () => {
-      currentPage = i;
-      renderImages();
-    });
-
-    paginationContainer.appendChild(pageButton);
-  }
+  });
 }
+
+// Initialize the page with the first set of images (page 1)
+fetchImages(currentPage);
